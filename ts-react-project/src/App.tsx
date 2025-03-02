4a6a771cb5cf3406df7f93450cadd1166c1f436f
shashwatrajhack <er.shashwatraj@gmail.com>
2025-03-03 03:37:46 +0530

    TypeScript interfaces and type

--- ts-react-project/src/App.tsx
+++ ts-react-project/src/App.tsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-     
+     <Todo>
+      title = "Hi there"
+      description = "Go to gym"
+      done = {false}
+     </Todo>
     </>
   )
+}
+
+interface TodoProp {
+  title:string,
+  description:string,
+  done:boolean
+};
+
+function Todo(props:TodoProp){
+  return <div>
+      <h1>{props.title}</h1>
+      <h2>{props.description}</h2>
+    </div>
+
 }
 
 export default App
